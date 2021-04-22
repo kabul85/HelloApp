66a53e7e1c2c27f12321a208d9b3fcb3541fd84e
kabul85 <35343882+kabul85@users.noreply.github.com>
2021-04-22 11:30:32 +0500

    Update Main.java

--- src/com/nuuz/Main.java
+++ src/com/nuuz/Main.java
@@ -3,6 +3,6 @@ package com.nuuz;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello Java from me");
     }
 }
